4f9a03e62fb660d6edadfd69152b87aac93a0410
NotBadCode <notbadcode.dev@gmail.com>
2022-02-08 22:06:54 +0100

    restucture proyect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rootPaths: ['./src'],
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 
-import CardList from "../common/components/cards/card-list/card-list.component";
+import CardList from "../../common/components/cards/card-list/card-list.component";
 
 import { ICard } from "../core/models/card.model";
 
