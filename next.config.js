4b457356c624daf3ab2588b5cb564f3fae6c3251
NotBadCode <notbadcode.dev@gmail.com>
2022-02-20 19:28:40 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: isProd ? 'https://cdn.statically.io/gh/notbadcode-dev/dev-things.github.io/gh-pages/' : '',
+    assetPrefix: isProd ? 'https://cdn.statically.io/gh/notbadcode-dev/notbadcode-dev.github.io/dev-things/gh-pages/' : '',
 }
 
 module.exports = nextConfig
