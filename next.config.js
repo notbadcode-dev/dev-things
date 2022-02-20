c752a1af81c2fa94181143e3797ca3c3e8d692e8
NotBadCode <notbadcode.dev@gmail.com>
2022-02-20 22:41:34 +0100

    refill integrate.yml

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: isProd ? 'https://cdn.statically.io/gh/notbadcode-dev/dev-things/gh-pages/' : '',
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
 
 module.exports = nextConfig
