3cf144da446e4b4112ea9987b9c715e87a31eb24
NotBadCode <notbadcode.dev@gmail.com>
2022-02-20 19:25:28 +0100

    add gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    assetPrefix: isProd ? 'https://cdn.statically.io/gh/notbadcode-dev/dev-things.github.io/gh-pages/' : '',
 }
 
 module.exports = nextConfig
