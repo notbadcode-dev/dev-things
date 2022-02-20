856d24ef5bddd284b00d1e0f5fc0f7ca1f06af80
NotBadCode <notbadcode.dev@gmail.com>
2022-02-20 19:00:20 +0100

    Fix compilation

--- src/pages/generate-qr/index.tsx
+++ src/pages/generate-qr/index.tsx
@@ -19,7 +19,7 @@ const GenerateQr = () => {
   const [value, setValue] = useState("");
 
   const qrCodeDowload = () => {
-    const svg = document.getElementById("QRCode");
+    const svg = document.getElementById("QRCode") as HTMLElement;
     const svgData = new XMLSerializer()?.serializeToString(svg);
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
@@ -27,7 +27,7 @@ const GenerateQr = () => {
     img.onload = () => {
       canvas.width = img.width;
       canvas.height = img.height;
-      ctx.drawImage(img, 0, 0);
+      ctx?.drawImage(img, 0, 0);
       const pngFile = canvas.toDataURL("image/png");
       const downloadLink = document.createElement("a");
       downloadLink.download = "QRCode";
