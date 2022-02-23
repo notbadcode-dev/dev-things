d85996faf7361844797c4365c6151fd008dc1238
NotBadCode <notbadcode.dev@gmail.com>
2022-02-23 22:35:13 +0100

    any changes

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
+const withTM = require("next-transpile-modules")([
+    "@adobe/react-spectrum"])
 
 const nextConfig = {
+    webpack: 4,
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_ASSETS_PREFIX,
-    trailingSlash: false,
     poweredByHeader: false,
     webpack(config, options) {
         const { isServer } = options;
         config.resolve.alias = {
             ...config.resolve.alias,
-            '@components': path.resolve('./components'),
+            '@components': path.resolve('./src/shared/components'),
             '@public': path.resolve('./public'),
-            '@redux': path.resolve('./redux'),
-            '@utils': path.resolve('./utils'),
         };
 
         config.module.rules.push({
@@ -25,7 +25,7 @@ const nextConfig = {
                     options: {
                         name: '[name]-[hash:8].[ext]',
                         publicPath: (url) => {
-                            return `/dev-things/_next/static/css/${url}`;
+                            return `/_next/static/css/${url}`;
                         },
                         outputPath: `${isServer ? '../' : ''}static/css/`,
                         esModule: false,
@@ -38,7 +38,6 @@ const nextConfig = {
     },
 };
 
-module.exports = withPlugins(
-    [withCSS],
-    nextConfig,
-);
+module.exports = {
+    reactStrictMode: true,
+}
