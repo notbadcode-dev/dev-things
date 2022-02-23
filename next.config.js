2fa2606d5a1528dd641f80b0b62d920f030aa359
NotBadCode <notbadcode.dev@gmail.com>
2022-02-23 21:51:02 +0100

    change config

--- next.config.js
+++ next.config.js
@@ -5,40 +5,40 @@ const withCSS = require('@zeit/next-css');
 const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_ASSETS_PREFIX,
-  trailingSlash: false,
-  poweredByHeader: false,
-  webpack(config, options) {
-    const { isServer } = options;
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@components': path.resolve('./components'),
-      '@public': path.resolve('./public'),
-      '@redux': path.resolve('./redux'),
-      '@utils': path.resolve('./utils'),
-    };
+    trailingSlash: false,
+    poweredByHeader: false,
+    webpack(config, options) {
+        const { isServer } = options;
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@components': path.resolve('./components'),
+            '@public': path.resolve('./public'),
+            '@redux': path.resolve('./redux'),
+            '@utils': path.resolve('./utils'),
+        };
 
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name]-[hash:8].[ext]',
-            publicPath: (url) => {
-              return `/_next/static/css/${url}`;
-            },
-            outputPath: `${isServer ? '../' : ''}static/css/`,
-            esModule: false,
-          },
-        },
-      ],
-    });
+        config.module.rules.push({
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name]-[hash:8].[ext]',
+                        publicPath: (url) => {
+                            return `/_next/static/css/${url}`;
+                        },
+                        outputPath: `${isServer ? '../' : ''}static/css/`,
+                        esModule: false,
+                    },
+                },
+            ],
+        });
 
-    return config;
-  },
+        return config;
+    },
 };
 
 module.exports = withPlugins(
-  [withCSS],
-  nextConfig,
+    [withCSS],
+    nextConfig,
 );
