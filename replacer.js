358d25094c03adb89f7ca6d0a19782111993640e
NotBadCode <notbadcode.dev@gmail.com>
2022-03-05 19:31:52 +0100

    Update replacer.js

--- replacer.js
+++ replacer.js
@@ -1,15 +1,28 @@
 const replace = require("replace-in-file");
-const options = {
+const options = [{
   //you may need to modify the file address to suite your project
   files: "./out/index.html",
   from: [/src="\//g, /href="\//g],
   to: ['src="./', 'href="./'],
-};
-(async function () {
-  try {
-    const results = await replace(options);
-    console.log("Replacement results:", results);
-  } catch (error) {
-    console.error("Error occurred:", error);
-  }
-})();
+}, {
+  //you may need to modify the file address to suite your project
+  files: "./out/generate-qr.html",
+  from: [/src="\//g, /href="\//g],
+  to: ['src="./', 'href="./'],
+}, {
+  //you may need to modify the file address to suite your project
+  files: "./out/shortener.html",
+  from: [/src="\//g, /href="\//g],
+  to: ['src="./', 'href="./'],
+}];
+
+options.forEach(option => {
+  (async function () {
+    try {
+      const results = await replace(option);
+      console.log("Replacement results:", results);
+    } catch (error) {
+      console.error("Error occurred:", error);
+    }
+  })();
+});
